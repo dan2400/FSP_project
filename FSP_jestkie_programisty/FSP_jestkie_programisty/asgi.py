3ee9e562ded6c8666a5a8343523b61cb5af54443
dan24_00 <dan24_00@mail.ru>
2024-11-20 15:17:16 +0300

    first

---
+++ FSP_jestkie_programisty/FSP_jestkie_programisty/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for FSP_jestkie_programisty project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "FSP_jestkie_programisty.settings")
+
+application = get_asgi_application()
